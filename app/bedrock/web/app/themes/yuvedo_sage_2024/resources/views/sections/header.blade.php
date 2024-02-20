be122703aa0980a21c598f7c640fd70a790ed9ab
wuwu <wuwu@users.noreply.github.com>
2024-02-20 20:41:27 +0100

    setup custom menu with log1x/navi composer plugin for sage theme

--- app/bedrock/web/app/themes/yuvedo_sage_2024/resources/views/sections/header.blade.php
+++ app/bedrock/web/app/themes/yuvedo_sage_2024/resources/views/sections/header.blade.php
@@ -1,4 +1,7 @@
 
+
+
+
 <header class="fixed top-12 left-0 w-full  z-50">
   <div class="mx-4 sm:mx-6 lg:mx-8  px-4 sm:px-6 lg:px-8 bg-white rounded-lg">
     <div class="flex justify-between items-center h-24 ">
@@ -8,9 +11,7 @@
         </a>
       </div>
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary hidden md:block" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
+    @include('partials.navigation')
   @endif
       <!-- Other content on the right side -->
       <div class="hidden md:block">
