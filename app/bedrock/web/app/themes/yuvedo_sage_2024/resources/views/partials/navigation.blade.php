92278f40cde0c66dadf80821447a54006dc26350
wuwu <wuwu@users.noreply.github.com>
2024-03-04 08:07:18 +0100

    feat: style navigation

--- app/bedrock/web/app/themes/yuvedo_sage_2024/resources/views/partials/navigation.blade.php
+++ app/bedrock/web/app/themes/yuvedo_sage_2024/resources/views/partials/navigation.blade.php
@@ -4,13 +4,13 @@
   <ul class="flex flex-wrap items-center list-none font-semibold text-lg">
     @foreach ($navigation as $item)
       @if ($item->children)
-        <li class="px-4 lg:pl-8 relative flex items-center space-x-1 {{ $item->classes ?? '' }} " x-data="{ open: false }" @mouseenter="open = true"
+        <li class="pr-4 lg:pr-4 underline-offset-2 relative flex items-center space-x-1 {{ $item->classes ?? '' }} " x-data="{ open: false }" @mouseenter="open = true"
       @mouseleave="open = false">
-          <a href="{{ $item->url }}" class="text-primary">
+          <a href="{{ $item->url }}" class="text-black hover:underline underline-offset-4">
             {{ $item->label }}
           </a>
       <ul
-        class="list-none origin-top-right absolute top-full left-1/2 -translate-x-1/2 min-w-[240px] bg-white border border-slate-200 p-2 rounded-lg shadow-xl [&[x-cloak]]:hidden"
+        class="list-none origin-top-right absolute top-full left-1/2 -translate-x-1/2 min-w-[240px] bg-white border border-slate-200 rounded-lg p-4 shadow-xl [&[x-cloak]]:hidden"
         x-show="open" x-transition:enter="transition ease-out duration-200 transform"
         x-transition:enter-start="opacity-0 -translate-y-2" x-transition:enter-end="opacity-100 translate-y-0"
         x-transition:leave="transition ease-out duration-200" x-transition:leave-start="opacity-100"
@@ -18,7 +18,7 @@
         @focusout="await $nextTick();!$el.contains($focus.focused()) && (open = false)">
             @foreach ($item->children as $child)
               <li class="my-child-item {{ $child->classes ?? '' }} {{ $child->active ? 'active' : '' }}">
-                <a href="{{ $child->url }}">
+                <a href="{{ $child->url }}" class="text-black hover:underline underline-offset-4">
                   {{ $child->label }}
                 </a>
               </li>
@@ -26,8 +26,8 @@
           </ul>
         </li>
       @else
-        <li class="px-4 lg:pl-8 {{ $item->classes ?? '' }} ">
-          <a href="{{ $item->url }}" class="text-primary">
+        <li class="pr-2 lg:pr-4 {{ $item->classes ?? '' }} ">
+          <a href="{{ $item->url }}" class="text-black hover:underline underline-offset-4">
             {{ $item->label }}
           </a>
         </li>
